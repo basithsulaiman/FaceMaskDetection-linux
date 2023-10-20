21f15a9d06e241c29bb2ca3572a24a0eb537db59
basithsulaiman <newtrobach@gmail.com>
2023-10-20 08:07:34 +0530

    2nd commit fixed for linux

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -26,7 +26,7 @@ INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
-DIRECTORY = r"/home/basith/Desktop/Codes/Face-Mask-Detection/dataset"
+DIRECTORY = r"./dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
 # grab the list of images in our dataset directory, then initialize
